034aee708b37dd2ae72b82943dcc9d7fcb07e8ae
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-04 18:40:30 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Books from "./pages/Books";
+import Home from "./pages/Home";
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/books">Books</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/books" element={<Books />} />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
